db6c196256464160a14ab5fc145e09b51eeb1010
Micho Dhani Firmansyah <michodhani01@gmail.com>
2024-04-06 18:34:30 +0700

    update scrapping
    Format republika.co.id diganti bjir

--- Scrap_Berita/republika.py
+++ Scrap_Berita/republika.py
@@ -10,30 +10,6 @@ soup = BeautifulSoup(response.text, 'html.parser')
 
 news_items = []
 
-def get_publish_time(publish_time):
-    if 'detik' in publish_time:
-        return datetime.now() - timedelta(seconds=int(publish_time.split()[0]))
-    elif 'menit' in publish_time:
-        return datetime.now() - timedelta(minutes=int(publish_time.split()[0]))
-    elif 'jam' in publish_time:
-        return datetime.now() - timedelta(hours=int(publish_time.split()[0]))
-    elif 'hari' in publish_time:
-        return datetime.now() - timedelta(days=int(publish_time.split()[0]))
-    elif 'minggu' in publish_time:
-        return datetime.now() - timedelta(weeks=int(publish_time.split()[0]))
-    elif 'bulan' in publish_time:
-        return datetime.now() - timedelta(weeks=int(publish_time.split()[0])*4)
-    elif 'tahun' in publish_time:
-        return datetime.now() - timedelta(weeks=int(publish_time.split()[0])*52)
-    else:
-        if 'minutes' in publish_time:
-            minutes_ago = int(publish_time.split()[0])
-            return datetime.now() - timedelta(minutes=minutes_ago)
-        elif 'hours' in publish_time:
-            hours_ago = int(publish_time.split()[0])
-            return datetime.now() - timedelta(hours=hours_ago)
-        else:
-            raise ValueError("Unknown time format: " + publish_time)
 # Find all article elements
 articles = soup.select('li.list-group-item.list-border.conten1')
 
@@ -47,17 +23,17 @@ for article in articles:
     # Kategori
     category = article.select_one('span.kanal-info').text.strip()
 
-    # Olah Waktu Publikasi
-    publish_time = get_publish_time(article.select_one('div.date').text.strip().split('-')[-1].strip())
+    # Mengambil teks dari tag <div class="date">
+    date_text = article.select_one('div.date').get_text(strip=True)
 
-    # Formatting Waktu Publikasi
-    publish_time_str = publish_time.strftime("%d-%m-%Y %H:%M")
+    # Memisahkan teks untuk mendapatkan hanya tanggal dan waktu
+    publish_time = date_text.split('-')[-1].strip()
 
     # Create a dictionary for the news item
     news_item = {
         "title": title,
         "category": category,
-        "publish_time": publish_time_str,
+        "publish_time": publish_time,
     }
 
     # Append the news item to the list
@@ -72,3 +48,34 @@ scrap_time = datetime.now().strftime("%d-%m-%Y %H:%M")
 
 with open('Scrap_Berita/Scrap_Time.json', 'w') as json_file:
     json.dump(scrap_time, json_file, indent=4) 
+    
+# def get_publish_time(publish_time):
+#     if 'detik' in publish_time:
+#         return datetime.now() - timedelta(seconds=int(publish_time.split()[0]))
+#     elif 'menit' in publish_time:
+#         return datetime.now() - timedelta(minutes=int(publish_time.split()[0]))
+#     elif 'jam' in publish_time:
+#         return datetime.now() - timedelta(hours=int(publish_time.split()[0]))
+#     elif 'hari' in publish_time:
+#         return datetime.now() - timedelta(days=int(publish_time.split()[0]))
+#     elif 'minggu' in publish_time:
+#         return datetime.now() - timedelta(weeks=int(publish_time.split()[0]))
+#     elif 'bulan' in publish_time:
+#         return datetime.now() - timedelta(weeks=int(publish_time.split()[0])*4)
+#     elif 'tahun' in publish_time:
+#         return datetime.now() - timedelta(weeks=int(publish_time.split()[0])*52)
+#     else:
+#         if 'minutes' in publish_time:
+#             minutes_ago = int(publish_time.split()[0])
+#             return datetime.now() - timedelta(minutes=minutes_ago)
+#         elif 'hours' in publish_time:
+#             hours_ago = int(publish_time.split()[0])
+#             return datetime.now() - timedelta(hours=hours_ago)
+#         else:
+#             raise ValueError("Unknown time format: " + publish_time)
+
+    # Formatting Waktu Publikasi
+    # publish_time_str = publish_time.strftime("%d-%m-%Y %H:%M")
+    
+    # # tanggal dan waktu saja
+    # publish_time = get_publish_time(article.select_one('div.date').text.strip().split('-')[-1].strip())
